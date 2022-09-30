cd07727faeedda4d4cd9426feb9bdd3ebf42d314
MauricioAires <mauricioaires015@gmail.com>
2022-09-30 18:00:27 -0300

    feat: adicionar signInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
@@ -12,6 +13,8 @@ export function Header() {
           </a>
           <a href="">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   )
---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,18 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+import S from './styles.module.scss'
+
+export function SignInButton() {
+  const isUserLoggedIn = true
+
+  return isUserLoggedIn ? (
+    <button className={S.signInButton}>
+      <FaGithub color="#04d361" /> Mauricio <FiX className={S.closeIcon} />
+    </button>
+  ) : (
+    <button className={S.signInButton}>
+      <FaGithub color="#eba417" /> Sing in wiht Github
+    </button>
+  )
+}
