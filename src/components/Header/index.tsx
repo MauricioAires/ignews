22232ec784c38fc61d05279affa5e1c1bdcc9679
MauricioAires <mauricioaires015@gmail.com>
2022-09-30 14:59:59 -0300

    feat: criando o coponente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Uma menina segurando um notebook" />
+
+        <nav>
+          <a className={styles.active} href="">
+            Home
+          </a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
