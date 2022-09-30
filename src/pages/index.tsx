63a4e3accb40336ded00e0509050bbe0199b0d3d
MauricioAires <mauricioaires015@gmail.com>
2022-09-30 14:20:13 -0300

    feat: configurando css global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
     <>
@@ -9,7 +7,7 @@ export default function Home() {
         <title>ig.news</title>
       </Head>
 
-      <h1 className={styles.title}>
+      <h1>
         Hello
         <span>world</span>
       </h1>
