2f1a098397788e7fdbd95b8d7319710ab5329752
MauricioAires <mauricioaires015@gmail.com>
2022-10-12 16:31:23 -0300

    feat: componente de activeLink

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,26 @@
+import Link, { LinkProps } from 'next/link'
+import { useRouter } from 'next/router'
+
+import { ReactElement, cloneElement } from 'react'
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement
+  activeClassName: string
+}
+
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter()
+
+  const className = asPath === rest.href ? activeClassName : ''
+  return (
+    <Link passHref {...rest}>
+      {cloneElement(children, {
+        className
+      })}
+    </Link>
+  )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,25 @@
 import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink'
+
 import { SignInButton } from '../SignInButton'
-import styles from './styles.module.scss'
+import S from './styles.module.scss'
 
 export function Header() {
   return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
+    <header className={S.headerContainer}>
+      <div className={S.headerContent}>
         <Link href="/">
           <img src="/images/logo.svg" alt="Uma menina segurando um notebook" />
         </Link>
 
         <nav>
-          <a className={styles.active} href="/">
-            Home
-          </a>
-          <a href="/posts">Posts</a>
+          <ActiveLink activeClassName={S.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={S.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
