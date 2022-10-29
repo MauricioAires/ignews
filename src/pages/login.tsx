d75f695fb4075e432161d4758921786070caeaee
MauricioAires <mauricioaires015@gmail.com>
2022-10-29 00:41:42 -0300

    feat: redirecionar usuários já logado

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,27 @@
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
 import { LoginTemplate } from '../templates/Login'
 
 export default function LoginPage() {
   return <LoginTemplate />
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getSession({
+    req
+  })
+
+  // se estiver logado mandar para home
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
