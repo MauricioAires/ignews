3e55cb7dc687b202467619d0aaa9b774044c9613
MauricioAires <mauricioaires015@gmail.com>
2022-11-01 01:01:19 -0300

    feat: adicionar dica de inscição

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -12,6 +12,14 @@ export interface HomeProps {
 }
 
 export function Home({ product }: HomeProps) {
+  /**
+   * está é a forma mais hardcore de pegar o ano atual e somar mais 1
+   */
+  const nextYear = new Date(
+    new Date().setFullYear(new Date().getFullYear() + 2)
+  ).toLocaleDateString('pt-BR', {
+    year: '2-digit'
+  })
   return (
     <>
       <Head>
@@ -29,7 +37,15 @@ export function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <div className={S.tip}>
+            <SubscribeButton priceId={product.priceId} />
+            <p aria-readonly>
+              <b>DICA:</b> preencha o formulário com o número do cartão (CVV)
+              <span>4242 4242 4242 4242</span>, data de validade
+              <time> 09/{nextYear}</time>, e código segurança (CVV)
+              <span>171</span>, para gerar uma inscrição valida.
+            </p>
+          </div>
         </section>
 
         <div className={S.banner}>
