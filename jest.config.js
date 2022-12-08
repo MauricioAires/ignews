4c3dcf8a578b10e8f5ac0534a84bd0e572ebd662
MauricioAires <mauricioaires015@gmail.com>
2022-12-08 00:12:13 -0300

    feat: config complice scss

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(scss|css|sass)$': 'identity-obj-proxy'
+  }
 }
